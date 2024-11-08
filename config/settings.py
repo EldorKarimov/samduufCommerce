51d243ea57f4b0db8ee14f72703dbcef08012e92
Eldor Karimov <hacktivist19021@gmail.com>
2024-11-08 11:37:38 +0500

    CSRF TRUSTED ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_DOMAINS', cast=Csv())
 
 
 # Application definition
